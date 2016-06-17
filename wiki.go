86f33b0833e9629f40ed9bd6ee5ee1889bf108f1
Jason Erickson <jason@stormpath.com>
2016-06-17 11:45:44 -0700

    Use identity

--- wiki.go
+++ wiki.go
@@ -10,13 +10,41 @@ import (
 	"strings"
 	"os"
 	"log"
+	"encoding/json"
 )
 
 var validPath = regexp.MustCompile("^/(view|edit|save|delete|admin)/([a-zA-Z0-9_]*)$")
 
+type Account struct {
+	Username string `json:"username"`
+	Email string `json:"email"`
+	GivenName string `json:"givenName"`
+	MiddleName string `json:"middleName"`
+	Surname string `json:"surname"`
+	FullName string `json:"fullName"`
+	Groups []string `json:"groups"`
+}
+
+func getIdentity(r *http.Request) *Account {
+	accountHeader := r.Header.Get("X-Forwarded-Account")
+	log.Println("Account Header: " + accountHeader)
+
+	if len(accountHeader) == 0 {
+		log.Println("Returning no account")
+		return nil
+	} else {
+		var account *Account
+		dec := json.NewDecoder(strings.NewReader(accountHeader))
+		dec.Decode(&account)
+		log.Printf("Returning Account %+v", account)
+		return account
+	}
+}
+
 type Page struct {
 	Title string
 	Body  []byte
+	Account *Account
 }
 
 func (p *Page) BodyStr() string {
@@ -60,7 +88,13 @@ func loadUserPage(title string) (*Page, error) {
 	return loadPage(title, filename)
 }
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+func makeUntitledHandler(fn func(http.ResponseWriter, *http.Request, *Account)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		account := getIdentity(r)
+		fn(w, r, account)
+	}
+}
+func makeTitledHandler(fn func(http.ResponseWriter, *http.Request, *Account, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("makeHandler handling " + r.URL.Path)
 		m := validPath.FindStringSubmatch(r.URL.Path)
@@ -70,7 +104,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 			http.NotFound(w, r)
 			return
 		}
-		fn(w, r, m[2])
+		fn(w, r, getIdentity(r), m[2])
 	}
 
 }
@@ -80,34 +114,35 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+func viewHandler(w http.ResponseWriter, r *http.Request, account *Account, title string) {
 	p, err := loadUserPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
 		return
 	}
-
+	p.Account = account
 	renderTemplate(w, "view.html", p)
 }
 
-func adminHandler(w http.ResponseWriter, r *http.Request) {
+func adminHandler(w http.ResponseWriter, r *http.Request, account *Account) {
 	title := "Admin"
-	renderTemplate(w, "admin.html", &Page{Title: title})
+	renderTemplate(w, "admin.html", &Page{Title: title, Account: account})
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+func editHandler(w http.ResponseWriter, r *http.Request, account *Account, title string) {
 	p, err := loadUserPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
+	p.Account = account
 	renderTemplate(w, "edit.html", p)
 }
 
-func createHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "create.html", &Page{Title: "Create New Page"})
+func createHandler(w http.ResponseWriter, r *http.Request, account *Account) {
+	renderTemplate(w, "create.html", &Page{Title: "Create New Page", Account: account})
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+func saveHandler(w http.ResponseWriter, r *http.Request, account *Account, title string) {
 	if len(title) == 0 {
 		title = r.FormValue("title")
 	}
@@ -121,7 +156,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
-func deleteHandler(w http.ResponseWriter, r *http.Request, title string) {
+func deleteHandler(w http.ResponseWriter, r *http.Request, account *Account, title string) {
 	if title == "FrontPage" {
 		http.Error(w, "You cannot delete the front page", http.StatusBadRequest)
 	}
@@ -133,6 +168,11 @@ func deleteHandler(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func renderTemplate(w http.ResponseWriter, templateName string, p *Page) {
+	if p.Account == nil {
+		log.Println("No account")
+	} else {
+		log.Println("Account for " + p.Account.FullName)
+	}
 	template, _ := raymond.ParseFile("templates/" + templateName)
 	template.RegisterPartialFile("templates/layout_top.mustache", "layout_top")
 	template.RegisterPartialFile("templates/layout_bottom.mustache", "layout_bottom")
@@ -143,12 +183,12 @@ func renderTemplate(w http.ResponseWriter, templateName string, p *Page) {
 func main() {
 	//fs := justFilesFilesystem{http.Dir("resources/")}
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
-	http.HandleFunc("/admin", adminHandler)
-	http.HandleFunc("/create/", createHandler)
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-	http.HandleFunc("/delete/", makeHandler(deleteHandler))
+	http.HandleFunc("/admin", makeUntitledHandler(adminHandler))
+	http.HandleFunc("/create/", makeUntitledHandler(createHandler))
+	http.HandleFunc("/view/", makeTitledHandler(viewHandler))
+	http.HandleFunc("/edit/", makeTitledHandler(editHandler))
+	http.HandleFunc("/save/", makeTitledHandler(saveHandler))
+	http.HandleFunc("/delete/", makeTitledHandler(deleteHandler))
 	http.HandleFunc("/", rootHandler)
 	http.ListenAndServe(":8080", nil)
 }
